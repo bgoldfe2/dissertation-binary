ab07ff1afc4c8906eb8a0af1007162b1e48d2ce1
bruce-goldfeder-asetpartners <bruce.goldfeder@asetpartners.com>
2023-08-23 18:39:50 -0400

    WIP loop set up needs os dir material

--- Scripts/analyze_traits_vs_notcb.py
+++ Scripts/analyze_traits_vs_notcb.py
@@ -11,88 +11,127 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 
-def parse_tvn():
+def parse_tvn(run_folder):
     #folder = 'Runs/2023-08-14_16_20_29--roberta-base/Ensemble/Output'
-    #file = '../Runs/2023-08-14_16_20_29--roberta-base/Ensemble/Output/ensemble-Age-test_acc-0.8619641547007652.csv'
+    file = '../Runs/2023-08-14_16_20_29--roberta-base/Ensemble/Output/ensemble-Age-test_acc-0.8619641547007652.csv'
     #file = '../Runs/2023-08-14_16_20_29--roberta-base/Ensemble/Output/ensemble-Ethnicity-test_acc-0.7924745833770045.csv'
     #file = '../Runs/2023-08-14_16_20_29--roberta-base/Ensemble/Output/ensemble-Gender-test_acc-0.8691961010376271.csv'
     #file = '../Runs/2023-08-14_16_20_29--roberta-base/Ensemble/Output/ensemble-Others-test_acc-0.6660727387066345.csv'
-    file = '../Runs/2023-08-14_16_20_29--roberta-base/Ensemble/Output/ensemble-Religion-test_acc-0.9057750759878419.csv'
+    #file = '../Runs/2023-08-14_16_20_29--roberta-base/Ensemble/Output/ensemble-Religion-test_acc-0.9057750759878419.csv'
 
+    ################# Outer Loop to read in all the files aka traits in Ensemble/Output and loop #################################
 
-    df = pd.read_csv(file)
-    total_all = len(df)
-    print(total_all)
-    print(df.columns)
-    # Number in per label that are correct
-    df['match'] = df['target']==df['y_pred']
-    #print(df)
+    # TODO loop through the folder and get each of the files starting with acc in the name or not metrics in the name?
+
+    fubar = [42]
     
-    #unmatched = pd.get_dummies(np.where(df['target']==df['y_pred'],'matched','unmatched')).sum(0)
-
-    #print(df[0:4]) 
-
-    # # Number in per label that are correct
-    # # also size(), count(), nth(), last()
-    count = df.groupby('label').size()
-    print("count is of type ", type(count))
-    print("keys are ", count.axes)
-    print("get value for Age ", count.get('Age'))
-    print("Size of each trait \n", count)
-
-    #df_group = df.groupby("label")
-    
+    for foo in fubar:    
         
-    print("confusion matrix for ","religion"," versus Not Cyberbullying")
-    cm = confusion_matrix(df['target'], df['y_pred'])
-    print("the type of the confusion matrix is ", type(cm))
-    # Print the confusion matrix
-    print(cm)
-    plt.figure()
-    sns.heatmap(cm, annot=True, fmt='d')
-    plt.xlabel('Predicted')
-    plt.ylabel('True')
-    #plt.show()
-    
-    cm_sum =  cm.sum()
-    print("sum of numbers in cm is ", cm_sum)
-    
-    df['false_pos'] = np.where(df['target']==0, 1, 0) & np.where(df['y_pred']==1, 1, 0)
-    df['false_neg'] = np.where(df['target']==1, 1, 0) & np.where(df['y_pred']==0, 1, 0)
-    df_cnt_fp = df.groupby('label')['false_pos'].apply(lambda x: (x==True).sum()).reset_index(name='count')
-    df_cnt_fn = df.groupby('label')['false_neg'].apply(lambda x: (x==True).sum()).reset_index(name='count')
-    
-    print('false positives')
-    print(df_cnt_fp)
-    print(type(df_cnt_fp))
-    print(df_cnt_fp.axes)
-    fp = df_cnt_fp.loc[df_cnt_fp['label']=='Religion', 'count'].values[0]
-    fn = df_cnt_fn.loc[df_cnt_fn['label']=='Religion', 'count'].values[0]
-    print(type(fp))
-    print(fp)
-    
-    print('false negatives')
-    print(df_cnt_fn)
+        # stand in for getting the file trait identifier during loop
+        file_trt = 'Age'
 
-    # Create each sub-confusion matrix of 2 x 2 for the five traits
-    # Test hard coded for religion
-    total_religion = count.get('Religion')  # 1575
-    print("total in religion is ", total_religion)
+        df = pd.read_csv(file)
+        total_all = len(df)
+        print(total_all)
+        print(df.columns)
+        # Number in per label that are correct
+        df['match'] = df['target']==df['y_pred']
+        
 
-    total_true_religion = cm[0][0]
-    total_true_notcb = cm[1][1]
-    # total_false_religion = df_cnt_fp.get(5)  #61
-    # total_false_notcb_in_religion_model = df_cnt_fn.get('false_neg') # 0
+        # Number in per label that are correct
+        count = df.groupby('label').size()
+        print("count is of type ", type(count))
+        print("keys are ", count.axes)
+        print("get value for Age ", count.get('Age'))
+        print("Size of each trait \n", count)
+
+        # Aggregate Confusion Matrix generation for the model        
+        print("confusion matrix for ","religion"," versus Not Cyberbullying")
+        cm = confusion_matrix(df['target'], df['y_pred'])
+        print("the type of the confusion matrix is ", type(cm))
+        # Print the confusion matrix
+        print(cm)
+        plt.figure(1)
+        sns.heatmap(cm, annot=True, fmt='d')
+        plt.xlabel('Predicted')
+        plt.ylabel('True')
+        #plt.show()  # Show all plots at the end - can be same for saving?
+        
+        # Check to see the numbers add to 9541 = size of the test set
+        cm_sum =  cm.sum()
+        print("sum of numbers in cm is ", cm_sum) 
+        
+        df['false_pos'] = np.where(df['target']==0, 1, 0) & np.where(df['y_pred']==1, 1, 0)
+        df['false_neg'] = np.where(df['target']==1, 1, 0) & np.where(df['y_pred']==0, 1, 0)
+        df_cnt_fp = df.groupby('label')['false_pos'].apply(lambda x: (x==True).sum()).reset_index(name='count')
+        df_cnt_fn = df.groupby('label')['false_neg'].apply(lambda x: (x==True).sum()).reset_index(name='count')
+        
+        print('false positives')
+        print(df_cnt_fp)
+        print(type(df_cnt_fp))
+        print(df_cnt_fp.axes)
+
+        
+        print("trait in loop is ", file_trt)
+
+        fp = df_cnt_fp.loc[df_cnt_fp['label']==file_trt, 'count'].values[0]
+        fn = df_cnt_fn.loc[df_cnt_fn['label']==file_trt, 'count'].values[0]
+        print(type(fp))
+        print(fp)
+        
+        print('false negatives')
+        print(df_cnt_fn)
+
+        # Create each sub-confusion matrix of 2 x 2 for the five traits
+        # Test hard coded for religion
+        total_religion = count.get(file_trt)  # 1575
+        print("total in religion is ", total_religion)
+
+        total_true_religion = cm[0][0]
+        total_true_notcb = cm[1][1]
+        # total_false_religion = df_cnt_fp.get(5)  #61
+        # total_false_notcb_in_religion_model = df_cnt_fn.get('false_neg') # 0
 
 
-    cm_religion = np.array([[total_true_religion, fp], [fn, total_true_notcb]])
-    
-    print(cm_religion)
+        cm_trt = np.array([[total_true_religion, fp], [fn, total_true_notcb]])
+        
+        print(cm_trt)
+
+        # Show religion sub confusion matrix
+        # plt.figure(2)
+        # sns.heatmap(cm_religion, annot=True, fmt='d')
+        # plt.xlabel('Predicted')
+        # plt.ylabel('True')
+        # plt.show()
+
+        # Show the distribution of false cyberbullying inferences that should have been Notcb
+        
+        plt.figure(2)
+        # Create a barplot
+        x = df_cnt_fp.iloc[:, 0].to_list()
+        y = df_cnt_fp.iloc[:, 1].to_list()
+        #y = cm_religion.T[1]
+        print(" x ", x, " y ", y)
+        plt.title("".join([file_trt, " vs Notcb Model, ", file_trt, " that were labelled Notcb"]))
+        plt.bar(x, y)
+
+        # Show the distribution of the Notcb inferences that should have been cyberbullying
+        plt.figure(3)
+        # Create a barplot
+        x = df_cnt_fn.iloc[:, 0].to_list()
+        y = df_cnt_fn.iloc[:, 1].to_list()
+        #y = cm_religion.T[1]
+        print(" x ", x, " y ", y)
+        plt.title("".join([file_trt, " vs Notcb Model, Notcb and all the rest that were labelled ", file_trt]))
+        plt.bar(x, y)
+
+        # Show the plot
+        plt.show()
 
     
 
 
 
 if __name__=="__main__":
-    parse_tvn()
+    parse_tvn('bar')
     #graph_by_trt(df, cm)
