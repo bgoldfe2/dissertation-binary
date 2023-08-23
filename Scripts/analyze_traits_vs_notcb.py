078f8adb558be0c2dbd2adfe3ed5751ef49c9a28
bruce-goldfeder-asetpartners <bruce.goldfeder@asetpartners.com>
2023-08-23 13:14:22 -0400

    WIP binary sets of confusion matrices

--- Scripts/analyze_traits_vs_notcb.py
+++ Scripts/analyze_traits_vs_notcb.py
@@ -6,6 +6,10 @@
 import pandas as pd
 import numpy as np
 from Model_Config import traits
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+import matplotlib.pyplot as plt
+
 
 def parse_tvn():
     #folder = 'Runs/2023-08-14_16_20_29--roberta-base/Ensemble/Output'
@@ -29,11 +33,24 @@ def parse_tvn():
 
     # # Number in per label that are correct
     # # also size(), count(), nth(), last()
-    count = df.groupby('label').size()
-    print(count)
+    #count = df.groupby('label').size()
+    #print(count)
 
     #df_group = df.groupby("label")
-    # df_group.get_group('Age')
+    
+        
+    print("confusion matrix for ","religion"," versus Not Cyberbullying")
+    cm = confusion_matrix(df['target'], df['y_pred'])
+
+    # Print the confusion matrix
+    print(cm)
+    plt.figure()
+    sns.heatmap(cm, annot=True, fmt='d')
+    plt.xlabel('Predicted')
+    plt.ylabel('True')
+    plt.show()
+    
+    #df_group.get_group('Age')
 
 
     # for name_of_group, contents_of_group in df_group:
@@ -43,9 +60,31 @@ def parse_tvn():
     # df.groupby('var1')['var2'].apply(lambda x: (x=='val').sum()).reset_index(name='count')
 
     # df_groupby = df.groupby('label')['match'].apply(lambda x: (x==True).sum()).reset_index(name='count')
-    df_count = df.groupby('label')['match'].apply(lambda x: (x==True).sum()).reset_index(name='count')
-    print(df_count)
-    print(type(df_count))
+
+    return df
+
+def graph_by_trt(df):
+
+    
+    #df_fp = df.groupby('label')['match'].apply(lambda x: (x==True).sum()).reset_index(name='count')
+    #df = df.assign(false_pos=df['target']==1 & df['y_pred']==0)
+    #df = df.assign(false_neg=df['target']==0 & df['y_pred']==1)
+    df['false_pos'] = np.where(df['target']==0, 1, 0) & np.where(df['y_pred']==1, 1, 0)
+    df['false_neg'] = np.where(df['target']==1, 1, 0) & np.where(df['y_pred']==0, 1, 0)
+    df_cnt_fp = df.groupby('label')['false_pos'].apply(lambda x: (x==True).sum()).reset_index(name='count')
+    df_cnt_fn = df.groupby('label')['false_neg'].apply(lambda x: (x==True).sum()).reset_index(name='count')
+    
+    print('false positives')
+    print(df_cnt_fp)
+    
+    print('false negatives')
+    print(df_cnt_fn)
+
+
+    
+
+
 
 if __name__=="__main__":
-    parse_tvn()
+    df = parse_tvn()
+    graph_by_trt(df)
